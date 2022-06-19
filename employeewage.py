0ee5946315ee41502e4f638b472e4ee68ff014fc
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-06-19 21:04:31 +0530

    [Ronit]Added : Check attendance employee is present or absent

--- employeewage.py
+++ employeewage.py
@@ -2,7 +2,44 @@
     @Name = Ronit kumar Patel
     @Title = Employee Wage
 """
+import logging
+import random
 
 print("********************************************************************************")
 print("<<<<<<<<<<<<----- Welcome to Employee Wage Computation Program----->>>>>>>>>>>>> ")
 print("********************************************************************************")
+
+log = '%(lineno)d ** %(asctime)s ** %(message)s'
+logging.basicConfig(filename='EmployeeWage.log', filemode='w', format=log, level=logging.DEBUG)
+
+
+class EmployeeWage:
+    def employee(self):
+        """
+        This function is work for employees attendance
+        is there take attendance using random
+        :return: nothing
+        """
+        logging.debug("Employee wage program is running perfectly.....")
+        try:
+            rand = random.randint(0, 1)
+            if rand == 0:
+                print("Employee is present")
+            else:
+                print("Employee is absent")
+        except Exception as e:
+            print(e)
+            logging.exception("There is something occurs please re-check the code")
+
+
+if __name__ == "__main__":
+    try:
+        obj = EmployeeWage()
+        obj.employee()
+    except Exception as e:
+        print(e)
+        logging.warning("Warning !!")
+    else:
+        print("Employee attendance ! ")
+    finally:
+        print("Thanks for checking your attendance ")
