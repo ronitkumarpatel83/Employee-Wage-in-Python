206b5981c8574da6a37233bfa1892266d0dd609d
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-06-19 22:25:49 +0530

    [Ronit]Added : Part-time daily wage

--- employeewage.py
+++ employeewage.py
@@ -24,12 +24,16 @@ class EmployeeWage:
         try:
             wage_per_hour = 20
             daily_work_hour = 8
-            rand = random.randint(0, 1)
+            part_time_hour = 4
+            rand = random.randint(0, 2)
             if rand == 0:
                 print("Employee is present")
                 daily_wage = daily_work_hour * wage_per_hour
                 print(f"The total wage of a day : {daily_wage}")
-
+            elif rand == 1:
+                print("Employee is Part-time")
+                daily_wage = part_time_hour * wage_per_hour
+                print(f"The total wage of a day : {daily_wage}")
             else:
                 print("Employee is absent")
                 daily_wage = daily_work_hour * 0
