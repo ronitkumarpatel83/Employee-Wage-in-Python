bf9d97337ea0e422d790896d69f7dc062f7e744d
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-06-28 12:39:04 +0530

    [Ronit]Added : Option

--- employeewage.py
+++ employeewage.py
@@ -30,13 +30,13 @@ class Employee:
         try:
             if rand == 0:
                 daily_work_hour = 8
-                print(" Employee is present ")
+                # print(" Employee is present ")
             elif rand == 1:
                 daily_work_hour = 4
-                print(" Employee is present for part-time ")
+                # print(" Employee is present for part-time ")
             else:
                 daily_work_hour = 0
-                print(" Employee is absent ")
+                # print(" Employee is absent ")
             return daily_work_hour
         except Exception as e:
             print(e)
@@ -57,15 +57,15 @@ class Employee:
                 no_of_working_days += 1
                 rand = random.randint(0, 2)
                 daily_work_hour = self.check_attendance(rand)
-                print(f" The working days is : {no_of_working_days}")
+                # print(f" The working days is : {no_of_working_days}")
                 working_hours += daily_work_hour
-                print(f" The working hours is : {working_hours}")
+                # print(f" The working hours is : {working_hours}")
                 daily_wage = self.wage_per_hour * daily_work_hour
-                print(f" The daily wage is : {daily_wage}")
+                # print(f" The daily wage is : {daily_wage}")
                 total_wage += daily_wage
-                print("------------------------------------------------")
-            print(f"The monthly wage is : {total_wage}")
-            print("------------------------------------------------")
+                # print("------------------------------------------------")
+            # print(f"The monthly wage is : {total_wage}")
+            # print("------------------------------------------------")
             return total_wage
 
         except Exception as e:
@@ -92,8 +92,12 @@ class Company:
     def delete_employee(self, emp_name):
         self.employee_dict.pop(emp_name)
 
-    def view(self):
-        print(self.employee_dict)
+    def employee_details_view(self):
+        for i in self.employee_dict:
+            # print(i)
+            # print(self.employee_dict.get(i))
+            emp_obj = self.employee_dict.get(i)
+            print(emp_obj.as_dict())
 
 
 def add_company():
@@ -138,13 +142,14 @@ def delete_employee():
     comp_e.delete_employee(emp_name)
 
 
-def display_employee():
+def display_employees():
     print("<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    c_name = input("Enter company name : ")
-    comp_e = comp_dict.get(c_name)
-    if comp_e is None:
-        print("Company doesn't exit")
-    comp_e.view()
+    company_name = input("Enter company name : ")
+    comp_obj = comp_dict.get(company_name)
+    if comp_obj is None:
+        print("Company doesn't exit ")
+        return
+    comp_obj.employee_details_view()
     print("<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 
@@ -152,14 +157,14 @@ if __name__ == "__main__":
     try:
         comp_dict = {}
         while True:
-            print(
-                "1.add_company\n2.Display Company\n3.Add Employee \n4.Get Employee\n5.Delete Employee\n6.View\n0.Exit")
+            print("1.add_company\n2.Display Company\n3.Add Employee \n4.Get Employee\n5.Delete "
+                  "Employee\n6.display_employees\n0.Exit")
             dict_e = {1: add_company,
                       2: display_company,
                       3: add_employee,
                       4: get_employee,
                       5: delete_employee,
-                      6: display_employee}
+                      6: display_employees}
 
             r = int(input("Enter a number : "))
             if r == 0:
